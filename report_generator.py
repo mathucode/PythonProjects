d10642289077a1252c80019bdf067738431ecf14
mathucode <125397081+mathucode@users.noreply.github.com>
2023-07-04 18:00:08 +0530

    Update report_generator.py

--- report_generator.py
+++ report_generator.py
@@ -1,17 +1,31 @@
 import matplotlib.pyplot as plt
-
+import pandas as pd
 from docxtpl import DocxTemplate, InlineImage
 from random import randint
 from docx2pdf import convert
+import openpyxl
 
 doc = DocxTemplate("reportTmpl.docx")
 
 salesRows = []
 topItems = []
+
+df = pd.read_excel(r"C:/Users/suvesh/Documents/data_items.xlsx")
+
+for index, row in df.iterrows():
+   sNo = row['S.No.']
+   name = row['Item Name']
+   costPu = row['Cost Per Unit']
+   nUnits = row['Units Sold']
+   salesRows.append({"sNo": sNo, "name": name, "cPu": costPu, "nUnits": nUnits, "revenue": costPu * nUnits})
+
+
+"""""
 for iItr in range(10):
   costPu = randint(1, 15)
   nUnits = randint(100, 500)
   salesRows.append({"sNo": iItr+1, "name": "Item "+str(iItr+1), "cPu": costPu, "nUnits": nUnits, "revenue": costPu*nUnits})
+"""
 
 topItems = [x["name"] for x in sorted(salesRows, key=lambda x: x["revenue"], reverse=True)][0:3]
 
